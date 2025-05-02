07e5d3ce0f6a354710a6ddbf2a856ac96a6c708b
Yasmina Attiallah El Hodhar <yasminaattiallahelhodhar@MacBook---Yas.local>
2025-05-02 07:53:37 +0200

    proxy setting for VPS youtube api call

--- services/youtube_transcript.py
+++ services/youtube_transcript.py
@@ -1,5 +1,6 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
+from youtube_transcript_api.proxies import GenericProxyConfig
 from urllib.parse import urlparse, parse_qs
 from utils.decorators import safe_exec, log_execution, timed
 
@@ -23,6 +24,10 @@ def get_transcript_text(video_url, languages=['fr', 'en']):
     video_id = extract_video_id(video_url)
     if not video_id:
         raise ValueError("Lien YouTube invalide.")
-
-    transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=languages)
+    
+    proxy = GenericProxyConfig(
+        http_url="http://brd-customer-hl_67d37d83-zone-residential_proxy1:zgxy3z6ptmb4@brd.superproxy.io:33335",
+        https_url="http://brd-customer-hl_67d37d83-zone-residential_proxy1:zgxy3z6ptmb4@brd.superproxy.io:33335"
+    )
+    transcript = YouTubeTranscriptApi(proxy_config=proxy).get_transcript(video_id, languages=languages)
     return "\n".join([entry['text'] for entry in transcript])
